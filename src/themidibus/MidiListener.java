ac37832ad316ad258c1706277b0f902852285b5a
Severin Smith <severin@smallbutdigital.com>
2017-11-06 14:06:24 -0500

    Switching to LGPL 3

--- src/themidibus/MidiListener.java
+++ src/themidibus/MidiListener.java
@@ -4,7 +4,7 @@
  * This file is part of a library called The MidiBus (themidibus) - http://www.smallbutdigital.com/themidibus.php.
  *
  * The MidiBus is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
@@ -13,7 +13,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with the MidiBus. If not, see <http://www.gnu.org/licenses/>.
 */
 
